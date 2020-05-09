ecda321d45d98f1f6828811a886161f8fe382fcc
Kailash Limba <kailash@culturro.com>
2020-05-09 15:39:46 +0530

    first comit

---
+++ hashmap.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+signed main(){
+
+    int n ;
+
+    vector<int> arr1, arr2;
+
+    for(int i =0;i<n;i++){
+
+        int x ;
+
+        arr1.push_back(x);
+    }
+        for(int i =0;i<n;i++){
+
+        int y;
+
+        arr2.push_back(y);
+    }
+
+   map<int, int> mymap;
+
+
+    for(int i =0;i<n;i++){
+        mymap[arr1[i]]= arr2[i];
+    }
+
+
+    return 0;
+}
